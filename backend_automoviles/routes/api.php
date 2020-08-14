88b6373c84c9c79a0fbb442eaea3b78e6d5f1b81
ccpcc1 <crisrxn12@gmail.com>
2020-08-14 12:22:13 -0500

    añadiendo ruta detalles

--- backend_automoviles/routes/api.php
+++ backend_automoviles/routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::Resource('/automoviles','AutomovilController')->names('Automoviles');
+Route::Resource('/automoviles','AutomovilController')->names('Automoviles')->parameters(['automoviles'=>automovil]);
